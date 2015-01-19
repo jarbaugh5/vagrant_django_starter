5f9279088aab8d369fecc85ff375adb19c9f9d45
Jon Arbaugh <jonathan.arbaugh@tufts.edu>
2015-01-19 16:42:57 -0500

    Get a basic django server working

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 sudo apt-get install -y python3-minimal
 sudo apt-get install -y python-virtualenv
+sudo apt-get install -y supervisor
 
 # Remove any former virtual environment
 VENV_DIR=/vagrant/env
@@ -20,3 +21,16 @@ PIP_FILE=${VENV_DIR}/bin/pip
 if [ -f "$REQ_FILE" ]; then
     ${PIP_FILE} install -r ${REQ_FILE}
 fi
+
+# Gunicorn should be a dependency, but just in case
+${PIP_FILE} install gunicorn
+
+# A line specific to Mimir's Django server
+# cp /vagrant/config/examples/config.py /vagrant/config/
+
+# Copy supervisor config file
+sudo cp /vagrant/gun_supervisor.conf /etc/supervisor/conf.d/gun_supervisor.conf
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo service supervisor restart
---
+++ gunicorn_config.py
@@ -0,0 +1,3 @@
+command='/vagrant/env/bin/gunicorn'
+pythonpath='/vagrant/env/bin'
+bind='0.0.0.0:8001'
